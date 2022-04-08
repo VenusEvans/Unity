28adee5ea5b0c768a001eec51be1126a7eb5c966
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-08 17:33:42 +0800

    Update PlayerAnimation.cs

--- About Animation/PlayerAnimation.cs
+++ About Animation/PlayerAnimation.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-
+    Rigidbody2D rb;
     Animator anim;
     PlayerMovement movement;
 
     int speedID;
     int isJumpID;
     int isCrouchID;
+    int isHurtID;
+    //public float jumpForce;
 
-    void Start()
+    void Start() 
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         movement = GetComponent<PlayerMovement>(); //为了让两个代码可以互相访问 要获取另一个代码（代码也是一个组件）
 
@@ -21,9 +24,9 @@ public class PlayerAnimation : MonoBehaviour
         speedID = Animator.StringToHash("Speed");
         isJumpID = Animator.StringToHash("isJump");
         isCrouchID = Animator.StringToHash("isCrouch");
+        isHurtID = Animator.StringToHash("isHurt");
     }
 
-    
     void Update()
     {
         AnimControl();
@@ -34,7 +37,28 @@ public class PlayerAnimation : MonoBehaviour
         anim.SetFloat(speedID, Mathf.Abs(movement.xVelocity));
         anim.SetBool(isJumpID, movement.isJump);
         anim.SetBool(isCrouchID, movement.isCrouch);
+        anim.SetBool(isHurtID, movement.isHurt);
+
     }
 
+    private void OnCollisionEnter2D(Collision2D collision) //与敌人碰撞 写在了动画脚本里有点乱 能力不到位 后期有能力后调整
+    {
 
+        if (collision.gameObject.tag == "Enemy") //如果碰撞的tag为Enemy
+        {
+            movement.isHurt = true;
+
+            if (anim.GetBool(isJumpID)) //如果是跳跃的动画
+            {
+                //rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime); //想让玩家在踩掉怪物后跳一下，不知道为什么不好使
+                Destroy(collision.gameObject);
+                anim.SetBool(isJumpID, movement.isJump);
+                movement.isHurt = false;
+            }
+        }
+        else
+        {
+            movement.isHurt = false;
+        }
+    }
 }
