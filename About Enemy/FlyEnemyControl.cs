f582997be5983e2af403d3a29b31c0c5222dcd31
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-08 17:37:43 +0800

    Create FlyEnemyControl.cs

---
+++ About Enemy/FlyEnemyControl.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*只是调整了一些参数，可以让敌人上下飞舞*/
+public class FlyEnemyControl: MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float speed; //敌人速度
+    public Transform up;
+    public Transform down;
+    bool FlyUp = true;
+    float upy;
+    float downy;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.DetachChildren();
+        upy = up.position.y;
+        downy = down.position.y;
+        Destroy(up.gameObject);
+        Destroy(down.gameObject);
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement() //敌人移动
+    {
+        if (FlyUp)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+            if (transform.position.y > upy)
+            {
+                //transform.localScale = new Vector3(-1, 1, 1);
+                FlyUp = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -speed);
+            if (transform.position.y < downy)
+            {
+                //transform.localScale = new Vector3(1, 1, 1);
+                FlyUp = true;
+            }
+        }
+    }
+}
