0275d8b9a5a0672475a7d9b36dd2d3a173802767
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-08 17:38:42 +0800

    Create Collection.cs

---
+++ About Collection/Collection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*简单的收集物品*/
+public class Collection : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) //收集物品 触发器
+    {
+        if(collision.tag== "Collection")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
