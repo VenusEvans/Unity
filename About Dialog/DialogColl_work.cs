954097ff0e7b796060356aa4cf84dd904c092429
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-10 18:46:53 +0800

    Create DialogColl_work.cs

---
+++ About Dialog/DialogColl_work.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*显示提示*/
+public class DialogColl_work: MonoBehaviour
+{
+    public GameObject tipDialog;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            tipDialog.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            tipDialog.SetActive(false);
+        }
+    }
+}
