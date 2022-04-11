687778cd119ca4f3466546b8dca60d790d2e80ed
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-11 18:44:07 +0800

    Update PlayerAnimation.cs

--- About Animation/PlayerAnimation.cs
+++ About Animation/PlayerAnimation.cs
@@ -7,13 +7,16 @@ public class PlayerAnimation : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
     PlayerMovement movement;
-
+    public int HP = 5;
     int speedID;
     int isJumpID;
     int isCrouchID;
     int isHurtID;
     public float jumpForce;
     public float force;
+
+    public bool isDie;
+    int isDieID;
 
     void Start() 
     {
@@ -26,6 +29,8 @@ public class PlayerAnimation : MonoBehaviour
         isJumpID = Animator.StringToHash("isJump");
         isCrouchID = Animator.StringToHash("isCrouch");
         isHurtID = Animator.StringToHash("isHurt");
+        isDieID = Animator.StringToHash("isDie");
+
     }
 
     void Update()
@@ -39,6 +44,7 @@ public class PlayerAnimation : MonoBehaviour
         anim.SetBool(isJumpID, movement.isJump);
         anim.SetBool(isCrouchID, movement.isCrouch);
         anim.SetBool(isHurtID, movement.isHurt);
+        anim.SetBool(isDieID, isDie);
 
     }
 
@@ -49,15 +55,30 @@ public class PlayerAnimation : MonoBehaviour
         {
             //EnemyControl_Gra deathAnim=collision.gameObject.GetComponent<EnemyControl_Gra>(); //调用其他脚本内的函数
             Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            movement.isHurt = true;
+            //movement.isHurt = true;
+            
 
             if (anim.GetBool(isJumpID)) //如果是跳跃的动画
             {
+                //movement.isHurt = false;
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce); //想让玩家在踩掉怪物后跳一下，不知道为什么不好使 /好了！！！！！！！！ 之间的deltaTime没用
                 //deathAnim.Death();
-                enemy.Death();
+                enemy.Death();               
                 anim.SetBool(isJumpID, movement.isJump);
-                movement.isHurt = false;
+
+            }else if (transform.position.x < collision.gameObject.transform.position.x)
+            {
+                HP--;
+                rb.velocity = new Vector2(-force, rb.velocity.y);
+                movement.isHurt = true;
+                AudioManager.PlayHitAudio();
+            }
+            else if (transform.position.x > collision.gameObject.transform.position.x)
+            {
+                HP--;
+                rb.velocity = new Vector2(force, rb.velocity.y);
+                movement.isHurt = true;
+                AudioManager.PlayHitAudio();
             }
         }
         else
