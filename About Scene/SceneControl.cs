a9d0102ee95ede4b37fa2b629b21defcc8e60e7f
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-12 19:06:08 +0800

    Update SceneControl.cs

--- About Scene/SceneControl.cs
+++ About Scene/SceneControl.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 public class SceneControl : MonoBehaviour
 {
@@ -11,6 +10,11 @@ public class SceneControl : MonoBehaviour
     void Awake()
     {
         //DontDestroyOnLoad(gameObject); //即使切换了场景 也不会摧毁当前游戏物体
+    }
+
+    public void ResetGame() //恢复游戏 暂停菜单后在进入下一关游戏还是停止的 需要在添加一个onclick事件
+    {
+        Time.timeScale = 1;
     }
 
     public void NextScene() //下一个场景
@@ -34,5 +38,30 @@ public class SceneControl : MonoBehaviour
     public void UpScene() //上一个场景
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame() //退出游戏
+    {
+        Application.Quit();
+    }
+
+    public void ReturnSelectLevel() //返回选择界面
+    {
+        SceneManager.LoadScene("SelectLevel");
+    }
+
+    public void Level1_1()
+    {
+        SceneManager.LoadScene("Level01_1");
+    }
+
+    public void Level1_2()
+    {
+        SceneManager.LoadScene("Level01_2");
+    }
+
+    public void Level1_3()
+    {
+        SceneManager.LoadScene("Level01_3");
     }
 }
