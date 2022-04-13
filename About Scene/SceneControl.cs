e4f4dd90f35840c64f0fd3f04f7ef76f1af0f09c
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-13 19:32:29 +0800

    Update SceneControl.cs

--- About Scene/SceneControl.cs
+++ About Scene/SceneControl.cs
@@ -12,14 +12,14 @@ public class SceneControl : MonoBehaviour
         //DontDestroyOnLoad(gameObject); //即使切换了场景 也不会摧毁当前游戏物体
     }
 
+    public void GoToSelectLevel()
+    {
+        SceneManager.LoadScene("SelectLevel");
+    }
+
     public void ResetGame() //恢复游戏 暂停菜单后在进入下一关游戏还是停止的 需要在添加一个onclick事件
     {
         Time.timeScale = 1;
-    }
-
-    public void NextScene() //下一个场景
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //加载当前游戏场景的下一个场景
     }
 
     public void OpenPauseMenu() //打开暂停菜单
@@ -35,33 +35,13 @@ public class SceneControl : MonoBehaviour
         Time.timeScale = 1;
     }
 
-    public void UpScene() //上一个场景
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-
     public void QuitGame() //退出游戏
     {
         Application.Quit();
     }
 
-    public void ReturnSelectLevel() //返回选择界面
+    public void Select(int levelIndex) //选择场景
     {
-        SceneManager.LoadScene("SelectLevel");
-    }
-
-    public void Level1_1()
-    {
-        SceneManager.LoadScene("Level01_1");
-    }
-
-    public void Level1_2()
-    {
-        SceneManager.LoadScene("Level01_2");
-    }
-
-    public void Level1_3()
-    {
-        SceneManager.LoadScene("Level01_3");
+        SceneManager.LoadScene(levelIndex);
     }
 }
