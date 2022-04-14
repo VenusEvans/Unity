4524a44390b827a32d1e4bc1324e599296e62c23
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-14 18:12:51 +0800

    Create EnemyControl_other.cs

---
+++ Other Scripts/EnemyControl_other.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*不動的食人花*/
+public class EnemyControl_other : Enemy //继承父级改为Enemy 为了调用消灭动画
+{
+    Rigidbody2D rb;
+
+    //public float speed; //敌人速度
+    //public Transform left;
+    //public Transform right;
+    //bool faceLeft = true;
+    //float leftx;
+    //float rightx;
+
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        //transform.DetachChildren();
+        //leftx = left.position.x;
+        //rightx = right.position.x;
+        //Destroy(left.gameObject);
+        //Destroy(right.gameObject);
+    }
+
+    //void Update()
+    //{
+    //    Movement();
+    //}
+
+    //void Movement() //敌人移动
+    //{
+    //    if (faceLeft)
+    //    {
+    //        rb.velocity = new Vector2(-speed, rb.velocity.y);
+    //        if (transform.position.x < leftx)
+    //        {
+    //            transform.localScale = new Vector3(-1, 1, 1);
+    //            faceLeft = false;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        rb.velocity = new Vector2(speed, rb.velocity.y);
+    //        if (transform.position.x > rightx)
+    //        {
+    //            transform.localScale = new Vector3(1, 1, 1);
+    //            faceLeft = true;
+    //        }
+    //    }
+    //}
+}
