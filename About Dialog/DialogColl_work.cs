6021df0d6d7cbb2a319e88bbcb6206e78e4d01c7
JianYe0525 <102718250+JianYe0525@users.noreply.github.com>
2022-04-19 15:37:19 +0800

    Update DialogColl_work.cs

--- About Dialog/DialogColl_work.cs
+++ About Dialog/DialogColl_work.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*显示提示*/
+
 public class DialogColl_work: MonoBehaviour
 {
     public GameObject tipDialog;
-
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
